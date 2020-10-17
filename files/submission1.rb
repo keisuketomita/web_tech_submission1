836e761fe3e372069969cfb47448d2e0e2505424
keisuketomita <keisuke.tommy@gmail.com>
2020-10-17 09:49:59 +0000

    課題提出

---
+++ files/submission1.rb
@@ -0,0 +1,12 @@
+require 'webrick'
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+})
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'submission1.html.erb')
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+server.start
